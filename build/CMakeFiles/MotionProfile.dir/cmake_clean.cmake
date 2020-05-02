49b7b3115868adf29d7df27857d7d03341f7a6db
salvamarce <salvatore.marcellini@gmail.com>
2020-05-02 20:14:36 +0200

    Primo commit

---
+++ build/CMakeFiles/MotionProfile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MotionProfile.dir/src/MotionProfile.cpp.o"
+  "../lib/libMotionProfile.pdb"
+  "../lib/libMotionProfile.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MotionProfile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
